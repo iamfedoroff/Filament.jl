1bc0277c94246e771b31041d5ec60fa428575628
fedoroff <devnull@localhost>
2018-06-14 12:33:07 +0300

    Specified types of function arguments in Fields module.

--- modules/Fields.jl
+++ modules/Fields.jl
@@ -3,6 +3,7 @@ module Fields
 using PyCall
 @pyimport scipy.constants as sc
 
+import Units
 import Grids
 import Fourier
 
@@ -23,7 +24,8 @@ mutable struct Field
 end
 
 
-function Field(unit, grid, lam0, initial_condition::Function)
+function Field(unit::Units.Unit, grid::Grids.Grid, lam0::Float64,
+               initial_condition::Function)
     f0 = C0 / lam0
     w0 = 2. * pi * f0
 
@@ -40,7 +42,7 @@ function Field(unit, grid, lam0, initial_condition::Function)
 end
 
 
-function peak_intensity(field)
+function peak_intensity(field::Field)
     Imax = maximum(abs2.(field.E))
     return Imax
 end
@@ -50,7 +52,7 @@ end
 Total energy:
     W = Int[|E(r,t)|^2 * 2*pi*r*dr*dt],   [W] = J
 """
-function energy(field)
+function energy(field::Field)
     r = field.grid.r
     W = 0.
     for j=1:field.grid.Nt
@@ -64,7 +66,7 @@ function energy(field)
 end
 
 
-function energy_gauss(field)
+function energy_gauss(field::Field)
     t0 = pulse_duration(field)
     a0 = beam_radius(field)
     I0 = peak_intensity(field)
@@ -73,7 +75,7 @@ function energy_gauss(field)
 end
 
 
-function energy_photon(field)
+function energy_photon(field::Field)
     Wph = HBAR * field.w0
     return Wph
 end
@@ -83,7 +85,7 @@ end
 Fluence:
     F(r) = Int[|E(r,t)|^2 * dt],   [F(r)] = J/cm^2
 """
-function fluence(field)
+function fluence(field::Field)
     F = zeros(field.grid.Nr)
     for j=1:field.grid.Nt
         for i=1:field.grid.Nr
@@ -95,14 +97,14 @@ function fluence(field)
 end
 
 
-function peak_fluence(field)
+function peak_fluence(field::Field)
     F = fluence(field)
     Fmax = maximum(F)
     return Fmax
 end
 
 
-function peak_fluence_gauss(field)
+function peak_fluence_gauss(field::Field)
     t0 = pulse_duration(field)
     I0 = peak_intensity(field)
     Fmaxg = sqrt(pi) * t0 * I0
@@ -110,7 +112,7 @@ function peak_fluence_gauss(field)
 end
 
 
-function beam_radius(field)
+function beam_radius(field::Field)
     F = fluence(field)
     a0 = 2. * radius(field.grid.r, F)
     # Factor 2. because F(r) is only half of full distribution F(x)
@@ -122,7 +124,7 @@ end
 Temporal fluence:
     F(t) = Int[|E(r,t)|^2 * 2*pi*r*dr],   [F(t)] = W
 """
-function temporal_fluence(field)
+function temporal_fluence(field::Field)
     r = field.grid.r
     F = zeros(field.grid.Nt)
     for j=1:field.grid.Nt
@@ -136,14 +138,14 @@ function temporal_fluence(field)
 end
 
 
-function pulse_duration(field)
+function pulse_duration(field::Field)
     F = temporal_fluence(field)
     t0 = radius(field.grid.t, F)
     return t0
 end
 
 
-function peak_power(field)
+function peak_power(field::Field)
     W = energy(field)
     t0 = pulse_duration(field)
     P = W / t0 / sqrt(pi)
@@ -151,7 +153,7 @@ function peak_power(field)
 end
 
 
-function peak_power_gauss(field)
+function peak_power_gauss(field::Field)
     a0 = beam_radius(field)
     I0 = peak_intensity(field)
     Pg = pi * a0^2 * I0
@@ -159,13 +161,13 @@ function peak_power_gauss(field)
 end
 
 
-function peak_plasma_density(field)
+function peak_plasma_density(field::Field)
     rhomax = maximum(field.rho)
     return rhomax
 end
 
 
-function plasma_radius(field)
+function plasma_radius(field::Field)
     rad = 2. * radius(field.grid.r, field.rho)
     # Factor 2. because rho(r) is only half of full distribution rho(x)
     return rad
@@ -176,7 +178,7 @@ end
 Linear plasma density:
     lrho = Int[Ne * 2*pi*r*dr],   [De] = 1/m
 """
-function linear_plasma_density(field)
+function linear_plasma_density(field::Field)
     r = field.grid.r
     lrho = 0.
     for i=1:field.grid.Nr
@@ -193,7 +195,7 @@ Integral power spectrum:
     Ew = FFT[Et]
     S = Int[|Ew|^2 * 2*pi*r*dr]
 """
-function integral_power_spectrum(field)
+function integral_power_spectrum(field::Field)
     r = field.grid.r
     S = zeros(field.grid.Nw)
     for i=1:field.grid.Nr
@@ -210,7 +212,7 @@ end
 
 
 """Step dx at a specific point i of a nonuniform grid x"""
-function step(i, x)
+function step(i::Int64, x::Array{Float64, 1})
     Nx = length(x)
     if i == 1
         dx = x[2] - x[1]
@@ -223,7 +225,7 @@ function step(i, x)
 end
 
 
-function radius(x, y, level=1. / exp(1.))
+function radius(x::Array{Float64, 1}, y::Array{Float64, 1}, level::Float64=1./e)
     Nx = length(x)
     ylevel = maximum(y) * level
 
