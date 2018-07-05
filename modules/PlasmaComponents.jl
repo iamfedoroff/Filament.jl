80f831aa3b802eb08edc74ba5a3ab8de955c7137
fedoroff <devnull@localhost>
2018-07-05 15:08:19 +0300

    Calculation of free charge for a specific component is moved to PlasmaComponents module.

--- modules/PlasmaComponents.jl
+++ modules/PlasmaComponents.jl
@@ -4,6 +4,7 @@ using PyCall
 @pyimport scipy.constants as sc
 
 import Units
+import Grids
 import Fields
 import Media
 import TabularFunctions
@@ -20,12 +21,21 @@ struct Component
     K :: Float64
     Wava :: Float64
     tf :: TabularFunctions.TabularFunction
+
+    rho0 :: Float64
+    rho :: Array{Float64, 1}
+    Kdrho :: Array{Float64, 1}
+    RI :: Array{Float64, 1}
+    keys :: Dict{String, Any}
 end
 
 
-function Component(unit::Units.Unit, field::Fields.Field, medium::Media.Medium,
-                   nuc::Float64, mr::Float64, name::String, frac::Float64,
-                   Ui::Float64, fname_tabfunc::String)
+function Component(unit::Units.Unit, grid::Grids.Grid, field::Fields.Field,
+                   medium::Media.Medium, rho0::Float64, nuc::Float64,
+                   mr::Float64, name::String, frac::Float64, Ui::Float64,
+                   fname_tabfunc::String, keys)
+
+    rho0 = rho0 / unit.rho
 
     Ui = Ui * QE   # eV -> J
     tf = TabularFunctions.TabularFunction(unit, fname_tabfunc)
@@ -38,7 +48,58 @@ function Component(unit::Units.Unit, field::Fields.Field, medium::Media.Medium,
     sigma = QE^2 / MR * nuc / (nuc^2 + field.w0^2)
     Wava = sigma / Ui * Eu^2 * unit.t
 
-    return Component(name, frac, Ui, K, Wava, tf)
+    rho = zeros(Float64, grid.Nt)
+    Kdrho = zeros(Float64, grid.Nt)
+    RI = zeros(Float64, grid.Nt)
+
+    return Component(name, frac, Ui, K, Wava, tf, rho0, rho, Kdrho, RI, keys)
+end
+
+
+"""
+Calculates free charge concentration, its derivative, and ionization rate for a
+given medium component.
+"""
+function free_charge(comp::Component, grid::Grids.Grid,
+                     Et::Array{Complex128, 1})
+    IONARG = comp.keys["IONARG"]
+    AVALANCHE = comp.keys["AVALANCHE"]
+
+    comp.rho[1] = 0.
+    comp.Kdrho[1] = 0.
+    comp.RI[1] = 0.
+
+    @inbounds for i=2:grid.Nt
+        if IONARG != 0
+            Ival = 0.5 * (abs2(Et[i]) + abs2(Et[i-1]))
+        else
+            Ival = 0.5 * (real(Et[i])^2 + real(Et[i-1])^2)
+        end
+
+        W1 = TabularFunctions.tfvalue(comp.tf, Ival)
+
+        if AVALANCHE != 0
+            W2 = comp.Wava * Ival
+        else
+            W2 = 0.
+        end
+
+        if W1 == 0.
+            # if no field ionization, then calculate only the avalanche one
+            comp.rho[i] = comp.rho[i-1] * exp(W2 * grid.dt)
+            comp.Kdrho[i] = 0.
+        else
+            # without this "if" statement 1/W12 will cause NaN values in Ne
+            W12 = W1 - W2
+            comp.rho[i] = W1 / W12 * comp.frac * comp.rho0 -
+                     (W1 / W12 * comp.frac * comp.rho0 - comp.rho[i-1]) *
+                     exp(-W12 * grid.dt)
+            comp.Kdrho[i] = comp.K * W1 * (comp.frac * comp.rho0 - comp.rho[i])
+        end
+
+        comp.RI[i] = W1
+    end
+    return nothing
 end
 
 
--- modules/Plasmas.jl
+++ modules/Plasmas.jl
@@ -5,11 +5,9 @@ import Grids
 import Fields
 import Media
 import PlasmaComponents
-import TabularFunctions
 
 
-mutable struct Plasma
-    rho0 :: Float64
+struct Plasma
     nuc :: Float64
     mr :: Float64
     components :: Array{PlasmaComponents.Component, 1}
@@ -17,14 +15,12 @@ mutable struct Plasma
     rho :: Array{Float64, 2}
     Kdrho :: Array{Float64, 2}
     RI :: Array{Float64, 2}
-    keys :: Dict{String, Any}
 end
 
 function Plasma(unit::Units.Unit, grid::Grids.Grid, field::Fields.Field,
                 medium::Media.Medium, rho0::Float64, nuc::Float64, mr::Float64,
                 components_dict::Array{Dict{String, Any}, 1},
                 keys)
-    rho0 = rho0 / unit.rho
 
     Ncomp = length(components_dict)
     components = Array{PlasmaComponents.Component}(Ncomp)
@@ -34,16 +30,16 @@ function Plasma(unit::Units.Unit, grid::Grids.Grid, field::Fields.Field,
         frac = comp_dict["fraction"]
         Ui = comp_dict["ionization_energy"]
         fname_tabfunc = comp_dict["tabular_function"]
-        components[i] = PlasmaComponents.Component(unit, field, medium, nuc, mr,
-                                                   name, frac, Ui,
-                                                   fname_tabfunc)
+        components[i] = PlasmaComponents.Component(unit, grid, field, medium,
+                                                   rho0, nuc, mr, name, frac,
+                                                   Ui, fname_tabfunc, keys)
     end
 
     rho = zeros(Float64, (grid.Nr, grid.Nt))
     Kdrho = zeros(Float64, (grid.Nr, grid.Nt))
     RI = zeros(Float64, (grid.Nr, grid.Nt))
 
-    return Plasma(rho0, nuc, mr, components, Ncomp, rho, Kdrho, RI, keys)
+    return Plasma(nuc, mr, components, Ncomp, rho, Kdrho, RI)
 end
 
 
@@ -80,61 +76,21 @@ Calculates free charge concentration, its derivative, and ionization rate for
 all medium components.
 """
 function free_charge(plasma, grid, field)
+    Et = zeros(Complex128, grid.Nt)
+
     @inbounds @. plasma.rho = 0.
     @inbounds @. plasma.Kdrho = 0.
     @inbounds @. plasma.RI = 0.
     for comp in plasma.components
         @inbounds for i=1:grid.Nr
-            Et = field.E[i, :]
-            rhot, drhot, RIt = free_charge_component(plasma, grid, Et, comp)
-            @views @. plasma.rho[i, :] = plasma.rho[i, :] + rhot
-            @views @. plasma.Kdrho[i, :] = plasma.Kdrho[i, :] + comp.K * drhot
-            @views @. plasma.RI[i, :] = plasma.RI[i, :] + RIt
+            @views @. Et = field.E[i, :]
+            PlasmaComponents.free_charge(comp, grid, Et)
+            @views @. plasma.rho[i, :] = plasma.rho[i, :] + comp.rho
+            @views @. plasma.Kdrho[i, :] = plasma.Kdrho[i, :] + comp.Kdrho
+            @views @. plasma.RI[i, :] = plasma.RI[i, :] + comp.RI
         end
     end
     @inbounds @views @. field.rho = plasma.rho[:, end]
-end
-
-
-"""
-Calculates free charge concentration, its derivative, and ionization rate for a
-specific medium component.
-"""
-function free_charge_component(plasma, grid, Et, comp)
-    It = zeros(Float64, grid.Nt)
-    rho = zeros(Float64, grid.Nt)
-    drho = zeros(Float64, grid.Nt)
-    RI = zeros(Float64, grid.Nt)
-
-    if plasma.keys["IONARG"] != 0
-        @inbounds @. It = abs2(Et)
-    else
-        @inbounds @. It = real(Et)^2
-    end
-
-    @inbounds for i=2:grid.Nt
-        Ival = 0.5 * (It[i] + It[i-1])
-        W1 = TabularFunctions.tfvalue(comp.tf, Ival)
-        if plasma.keys["AVALANCHE"] != 0
-            W2 = comp.Wava * Ival
-        else
-            W2 = 0.
-        end
-        if W1 == 0.
-            # if no field ionization, then calculate only the avalanche one
-            rho[i] = rho[i-1] * exp(W2 * grid.dt)
-            drho[i] = 0.
-        else
-            # without this "if" statement 1/W12 will cause NaN values in Ne
-            W12 = W1 - W2
-            rho[i] = W1 / W12 * comp.frac * plasma.rho0 -
-                     (W1 / W12 * comp.frac * plasma.rho0 - rho[i-1]) *
-                     exp(-W12 * grid.dt)
-            drho[i] = W1 * (comp.frac * plasma.rho0 - rho[i])
-        end
-        RI[i] = W1
-    end
-    return rho, drho, RI
 end
 
 
