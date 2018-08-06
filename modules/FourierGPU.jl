fae58552c602125348e6b86e1c85cb595103e4de
fedoroff <devnull@localhost>
2018-08-06 18:57:07 +0300

    Optimizations for FourierGPU module.

--- modules/FourierGPU.jl
+++ modules/FourierGPU.jl
@@ -13,6 +13,8 @@ struct FourierTransform
     Nt :: Int64
     Nw :: Int64
     HS_gpu :: CuArrays.CuArray{FloatGPU, 1}
+    Ec_gpu :: CuArrays.CuArray{ComplexGPU, 1}
+    Er_gpu :: CuArrays.CuArray{FloatGPU, 1}
     Sc_gpu :: CuArrays.CuArray{ComplexGPU, 1}
     Sr_gpu :: CuArrays.CuArray{ComplexGPU, 1}
     PFFT :: Base.DFT.Plan
@@ -34,6 +36,8 @@ function FourierTransform(Nt, dt)
     HS_gpu = CuArrays.CuArray(convert(Array{FloatGPU, 1}, HS))
 
     # arrays to store intermediate results:
+    Ec_gpu = CuArrays.CuArray(zeros(ComplexGPU, Nt))
+    Er_gpu = CuArrays.CuArray(zeros(FloatGPU, Nt))
     Sc_gpu = CuArrays.CuArray(zeros(ComplexGPU, Nt))
     Sr_gpu = CuArrays.CuArray(zeros(ComplexGPU, Nw))
 
@@ -44,26 +48,27 @@ function FourierTransform(Nt, dt)
     PRFFT = plan_rfft(CuArrays.CuArray(zeros(FloatGPU, Nt)))
     PIRFFT = plan_irfft(CuArrays.CuArray(zeros(ComplexGPU, Nw)), Nt)
 
-    # E_gpu = CuArrays.CuArray(zeros(FloatGPU, Nt))
-    # S_gpu = CuArrays.CuArray(zeros(ComplexGPU, Nw))
-    # A_mul_B!(S_gpu, PRFFT, E_gpu)
+    # A_mul_B!(Sc_gpu, PFFT, Ec_gpu)
+    # A_mul_B!(Ec_gpu, PIFFT, Sc_gpu)
+    # A_mul_B!(Sr_gpu, PRFFT, Er_gpu)
+    # A_mul_B!(Er_gpu, PIRFFT, Sr_gpu)
 
-    return FourierTransform(Nt, Nw, HS_gpu, Sc_gpu, Sr_gpu,
+    return FourierTransform(Nt, Nw, HS_gpu, Ec_gpu, Er_gpu, Sc_gpu, Sr_gpu,
                             PFFT, PIFFT, PRFFT, PIRFFT)
 end
 
 
-function fft1d!(FT::FourierTransform, Ec::Array{Complex128, 1},
-                Sc::Array{Complex128, 1})
-    A_mul_B!(Sc, FT.PFFT, Ec)
+function fft1d!(FT::FourierTransform, Ec_gpu::CuArrays.CuArray{ComplexGPU, 1},
+                Sc_gpu::CuArrays.CuArray{ComplexGPU, 1})
+    A_mul_B!(Sc_gpu, FT.PFFT, Ec_gpu)
     return nothing
 end
 
 
-function fft1d(FT::FourierTransform, Ec::Array{Complex128, 1})
-    Sc = zeros(Complex128, FT.Nt)
-    fft1d!(FT, Ec, Sc)   # time -> frequency
-    return Sc
+function fft1d(FT::FourierTransform, Ec_gpu::CuArrays.CuArray{ComplexGPU, 1})
+    Sc_gpu = CuArrays.CuArray(zeros(ComplexGPU, FT.Nt))
+    fft1d!(FT, Ec_gpu, Sc_gpu)   # time -> frequency
+    return Sc_gpu
 end
 
 
@@ -74,10 +79,10 @@ function ifft1d!(FT::FourierTransform, Sc_gpu::CuArrays.CuArray{ComplexGPU, 1},
 end
 
 
-function ifft1d(FT::FourierTransform, Sc::Array{Complex128, 1})
-    Ec = zeros(Complex128, FT.Nt)
-    ifft1d!(FT, Sc, Ec)   # frequency -> time
-    return Ec
+function ifft1d(FT::FourierTransform, Sc_gpu::CuArrays.CuArray{ComplexGPU, 1})
+    Ec_gpu = CuArrays.CuArray(zeros(ComplexGPU, FT.Nt))
+    ifft1d!(FT, Sc_gpu, Ec_gpu)   # frequency -> time
+    return Ec_gpu
 end
 
 
@@ -97,13 +102,11 @@ end
 
 function rfft2d!(FT::FourierTransform, E_gpu::CuArrays.CuArray{ComplexGPU, 2},
                  S_gpu::CuArrays.CuArray{ComplexGPU, 2})
-    Nr, Nt = size(E_gpu)
-    Et_gpu = CuArrays.CuArray(zeros(FloatGPU, FT.Nt))
-    St_gpu = CuArrays.CuArray(zeros(ComplexGPU, FT.Nw))
+    Nr = size(E_gpu, 1)
     for i=1:Nr
-        @inbounds @. Et_gpu = real(E_gpu[i, :])
-        rfft1d!(FT, Et_gpu, St_gpu)   # time -> frequency
-        @inbounds S_gpu[i, :] = St_gpu
+        @inbounds @. FT.Er_gpu = real(E_gpu[i, :])
+        rfft1d!(FT, FT.Er_gpu, FT.Sr_gpu)   # time -> frequency
+        @inbounds S_gpu[i, :] = FT.Sr_gpu
     end
     return nothing
 end
@@ -116,33 +119,33 @@ function irfft1d!(FT::FourierTransform, Sr_gpu::CuArrays.CuArray{ComplexGPU, 1},
 end
 
 
-function irfft1d(FT::FourierTransform, Sr::Array{Complex128, 1})
-    Er = zeros(Float64, FT.Nt)
-    irfft1d!(FT, Sr, Er)   # frequency -> time
-    return Er
+function irfft1d(FT::FourierTransform, Sr_gpu::CuArrays.CuArray{ComplexGPU, 1})
+    Er_gpu = CuArrays.CuArray(zeros(Float64, FT.Nt))
+    irfft1d!(FT, Sr_gpu, Er_gpu)   # frequency -> time
+    return Er_gpu
 end
 
 
 """Real time signal -> analytic time signal."""
 function signal_real_to_signal_analytic(FT::FourierTransform,
-                                        Er::Array{Float64, 1})
+                                        Er_gpu::CuArrays.CuArray{FloatGPU, 1})
     # Need test for odd N and low frequencies
-    S = fft(Er)
-    @. S = FT.HS * S
-    Ea = ifft(S)
-    return Ea
+    S_gpu = fft1d(FT, Er_gpu)
+    @. S_gpu = FT.HS_gpu * S_gpu
+    Ec_gpu = ifft1d(FT, S_gpu)
+    return Ec_gpu
 end
 
 
 """Spectrum of real time signal -> analytic time signal."""
 function spectrum_real_to_signal_analytic!(FT::FourierTransform,
                                            Sr_gpu::CuArrays.CuArray{ComplexGPU, 1},
-                                           Ea_gpu::CuArrays.CuArray{ComplexGPU, 1})
+                                           Ec_gpu::CuArrays.CuArray{ComplexGPU, 1})
     # Need test for odd N and low frequencies
     # S = vcat(Sr, conj(Sr[end-1:-1:2]))
     @inbounds FT.Sc_gpu[1:FT.Nw] = Sr_gpu
     @inbounds @. FT.Sc_gpu = FT.HS_gpu * FT.Sc_gpu
-    ifft1d!(FT, FT.Sc_gpu, Ea_gpu)
+    ifft1d!(FT, FT.Sc_gpu, Ec_gpu)
     return nothing
 end
 
@@ -150,13 +153,11 @@ end
 function spectrum_real_to_signal_analytic_2d!(FT::FourierTransform,
                                              S_gpu::CuArrays.CuArray{ComplexGPU, 2},
                                              E_gpu::CuArrays.CuArray{ComplexGPU, 2})
-    Nr, Nt = size(E_gpu)
-    St_gpu = CuArrays.CuArray(zeros(ComplexGPU, FT.Nw))
-    Et_gpu = CuArrays.CuArray(zeros(ComplexGPU, FT.Nt))
+    Nr = size(E_gpu, 1)
     for i=1:Nr
-        @inbounds St_gpu = S_gpu[i, :]
-        spectrum_real_to_signal_analytic!(FT, St_gpu, Et_gpu)
-        @inbounds E_gpu[i, :] = Et_gpu
+        @inbounds Sr_gpu = S_gpu[i, :]
+        spectrum_real_to_signal_analytic!(FT, Sr_gpu, FT.Ec_gpu)
+        @inbounds E_gpu[i, :] = FT.Ec_gpu
     end
 end
 
