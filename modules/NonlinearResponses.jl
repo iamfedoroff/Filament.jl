05c8c47f292455237d151dd147bcbe2859e59a1f
fedoroff <devnull@localhost>
2019-04-09 16:06:30 +0300

    Clean NonlinearResponses module.

--- modules/NonlinearResponses.jl
+++ modules/NonlinearResponses.jl
@@ -1,28 +1,5 @@
 module NonlinearResponses
 
-    import CUDAnative
-    import CuArrays
-    import CUDAdrv
-    import FFTW
-
-    import PyCall
-
-    import Units
-    import Fields
-    import Media
-    import Fourier
-
-    scipy_constants = PyCall.pyimport("scipy.constants")
-    const C0 = scipy_constants.c   # speed of light in vacuum
-    const EPS0 = scipy_constants.epsilon_0   # the electric constant (vacuum permittivity) [F/m]
-    const MU0 = scipy_constants.mu_0   # the magnetic constant [N/A^2]
-    const QE = scipy_constants.e   # elementary charge [C]
-    const ME = scipy_constants.m_e   # electron mass [kg]
-    const HBAR = scipy_constants.hbar   # the Planck constant (divided by 2*pi) [J*s]
-
-    const FloatGPU = Float32
-    const ComplexGPU = ComplexF32
-
 
     struct NonlinearResponse{T}
         Rnl :: T
@@ -35,5 +12,6 @@ module NonlinearResponses
         nresp.func(z, F, E, nresp.p)
         return nothing
     end
+
 
 end
