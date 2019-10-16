e7b0017b37fc61d0fb2ede5a9bd7c1f2a485beb6
iamfedoroff <iamfedoroff@gmail.com>
2019-10-16 12:51:11 +0300

    Advanced tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
-println()
-println("Running tests...")
-println()
+using Test
 
-run(`make`)
+PROJECT = joinpath(@__DIR__, "..")
+
+@testset "Run examples" begin
+    examples = ["R", "T", "XY", "RT", "RT_lattice"]
+    for example in examples
+        cd(joinpath(PROJECT, "examples", example))
+        proc = run(
+            `julia -O3 --check-bounds=no $PROJECT/src/Filament.jl input.jl`
+        )
+        @test proc.exitcode == 0
+    end
+end
