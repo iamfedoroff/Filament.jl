801dd8498e2c766ddad50817d4e7cb97d7bad803
iamfedoroff <iamfedoroff@gmail.com>
2020-08-13 16:47:14 +0300

    Simplifications in TabulatedFunctions.

--- src/modules/TabulatedFunctions.jl
+++ src/modules/TabulatedFunctions.jl
@@ -49,32 +49,15 @@ function (tf::TFunction)(x::T) where T<:AbstractFloat
     else
         if T == Float32   # FIXME Dirty hack for launching on both CPU and GPU
             xlog10 = CUDA.log10(x)
-            ylog10 = tfbase(tf, xlog10)
+            ylog10 = linterp(xlog10, tf.x, tf.y)
             res = CUDA.pow(convert(T, 10), ylog10)
         else
             xlog10 = log10(x)
-            ylog10 = tfbase(tf, xlog10)
+            ylog10 = linterp(xlog10, tf.x, tf.y)
             res = 10.0^ylog10
         end
     end
     return res
-end
-
-
-function tfbase(tf::TFunction, x::AbstractFloat)
-    dx = tf.x.step
-    if x <= tf.x[1]
-        dydx = (tf.y[2] - tf.y[1]) / dx
-        y = tf.y[2] + dydx * (x - tf.x[2])
-    elseif x >= tf.x[end]
-        dydx = (tf.y[end] - tf.y[end - 1]) / dx
-        y = tf.y[end - 1] + dydx * (x - tf.x[end - 1])
-    else
-        i = findindex(tf.x, x)
-        dydx = (tf.y[i + 1] - tf.y[i]) / dx
-        y = tf.y[i] + dydx * (x - tf.x[i])
-    end
-    return y
 end
 
 
@@ -93,10 +76,10 @@ function dtf(tf::TabulatedFunctions.TFunction, x::AbstractFloat)
         yN = dtfongrid(tf, N)
         yNm1 = dtfongrid(tf, N - 1)
         dydx = (yN - yNm1) / dx
-        res = yNm1 + dydx * (x - tf.x[end - 1])
+        res = yNm1 + dydx * (x - tf.x[end-1])
     else
-        i = findindex(tf.x, x)
-        yip1 = dtfongrid(tf, i + 1)
+        i = Int(cld(x - tf.x[1], dx))   # number of steps from x[1] to x
+        yip1 = dtfongrid(tf, i+1)
         yi = dtfongrid(tf, i)
         dydx = (yip1 - yi) / dx
         res = yi + dydx * (x - tf.x[i])
@@ -116,24 +99,18 @@ function dtfongrid(tf::TabulatedFunctions.TFunction, i::Int)
     h = tf.x.step
     if i <= 2
         c = @. convert(T, (-25/12, 4, -3, 4/3, -1/4))
-        res = (c[1] * tf.y[i] + c[2] * tf.y[i + 1] + c[3] * tf.y[i + 2] +
-               c[4] * tf.y[i + 3] + c[5] * tf.y[i + 4]) / h
+        res = (c[1] * tf.y[i] + c[2] * tf.y[i+1] + c[3] * tf.y[i+2] +
+               c[4] * tf.y[i+3] + c[5] * tf.y[i+4]) / h
     elseif i >= N - 2
         c = @. convert(T, (25/12, -4, 3, -4/3, 1/4))
-        res = (c[1] * tf.y[i] + c[2] * tf.y[i - 1] + c[3] * tf.y[i - 2] +
-               c[4] * tf.y[i - 3] + c[5] * tf.y[i - 4]) / h
+        res = (c[1] * tf.y[i] + c[2] * tf.y[i-1] + c[3] * tf.y[i-2] +
+               c[4] * tf.y[i-3] + c[5] * tf.y[i-4]) / h
     else
         c = @. convert(T, (1/12, -2/3, 0, 2/3, -1/12))
-        res = (c[1] * tf.y[i - 2] + c[2] * tf.y[i - 1] + c[4] * tf.y[i + 1] +
-               c[5] * tf.y[i + 2]) / h
+        res = (c[1] * tf.y[i-2] + c[2] * tf.y[i-1] + c[4] * tf.y[i+1] +
+               c[5] * tf.y[i+2]) / h
     end
     return res
-end
-
-
-function findindex(x::AbstractArray{T}, xc::T) where T<:AbstractFloat
-    ldx = (xc - x[1]) / (x[2] - x[1])   # number of steps dx from x[1] to xc
-    return Int(floor(ldx)) + 1
 end
 
 
@@ -141,16 +118,18 @@ end
 Linear interpolation on a grid with the constant step.
 """
 function linterp(t::AbstractFloat, tt::AbstractArray, ff::AbstractArray)
+    dt = tt[2] - tt[1]
     if t <= tt[1]
-        f = ff[1]
+        dfdt = (ff[2] - ff[1]) / dt
+        f = ff[2] + dfdt * (t - tt[2])
     elseif t >= tt[end]
-        f = ff[end]
+        dfdt = (ff[end] - ff[end-1]) / dt
+        f = ff[end-1] + dfdt * (t - tt[end-1])
     else
-        Nt = length(tt)
-        dt = tt[2] - tt[1]
         i = Int(cld(t - tt[1], dt))   # number of steps from tt[1] to t
-        i = min(i, Nt-1)   # extra safety
-        f = ff[i] + (ff[i+1] - ff[i]) / (tt[i+1] - tt[i]) * (t - tt[i])
+        i = min(i, length(tt)-1)   # extra safety
+        dfdt = (ff[i+1] - ff[i]) / dt
+        f = ff[i] + dfdt * (t - tt[i])
     end
     return f
 end
