323313a117625e0174b113a21d289256533b8745
iamfedoroff <iamfedoroff@gmail.com>
2020-11-12 16:52:18 +0300

    Version '3.0' of plot.hdf files.

--- src/modules/WritePlots.jl
+++ src/modules/WritePlots.jl
@@ -217,7 +217,7 @@ end
 # ******************************************************************************
 # PlotHDF
 # ******************************************************************************
-const PFVERSION = "2.0"
+const PFVERSION = "3.0"
 const GROUP_UNIT = "units"
 const GROUP_GRID = "grid"
 const GROUP_FDAT = "field"
@@ -254,11 +254,11 @@ function PlotHDF(
 
     fp = HDF5.h5open(fname, "w")
     fp["version"] = PFVERSION
-    _write_group_unit(fp, unit)
-    _write_group_grid(fp, grid)
+    _group_unit(fp, unit)
+    _group_grid(fp, grid)
     HDF5.g_create(fp, GROUP_FDAT)
     if ZDATA
-        _write_group_zdat(fp, grid)
+        _group_zdat(fp, grid)
     end
     HDF5.close(fp)
 
@@ -281,202 +281,21 @@ function PlotHDF(
 end
 
 
-function _write_group_unit(fp, unit::Units.UnitR)
-    HDF5.g_create(fp, GROUP_UNIT)
-    group = fp[GROUP_UNIT]
-    group["r"] = unit.r
-    group["z"] = unit.z
-    group["I"] = unit.I
-    return nothing
-end
-
-
-function _write_group_unit(fp, unit::Units.UnitT)
-    HDF5.g_create(fp, GROUP_UNIT)
-    group = fp[GROUP_UNIT]
-    group["z"] = unit.z
-    group["t"] = unit.t
-    group["I"] = unit.I
-    group["Ne"] = unit.rho
-    return nothing
-end
-
-
-function _write_group_unit(fp, unit::Units.UnitRT)
-    HDF5.g_create(fp, GROUP_UNIT)
-    group = fp[GROUP_UNIT]
-    group["r"] = unit.r
-    group["z"] = unit.z
-    group["t"] = unit.t
-    group["I"] = unit.I
-    group["Ne"] = unit.rho
-    return nothing
-end
-
-
-function _write_group_unit(fp, unit::Units.UnitXY)
-    HDF5.g_create(fp, GROUP_UNIT)
-    group = fp[GROUP_UNIT]
-    group["x"] = unit.x
-    group["y"] = unit.y
-    group["z"] = unit.z
-    group["I"] = unit.I
-    return nothing
-end
-
-
-function _write_group_unit(fp, unit::Units.UnitXYT)
-    HDF5.g_create(fp, GROUP_UNIT)
-    group = fp[GROUP_UNIT]
-    group["x"] = unit.x
-    group["y"] = unit.y
-    group["z"] = unit.z
-    group["t"] = unit.t
-    group["I"] = unit.I
-    group["Ne"] = unit.rho
-    return nothing
-end
-
-
-function _write_group_grid(fp, grid::Grids.GridR)
-    HDF5.g_create(fp, GROUP_GRID)
-    group = fp[GROUP_GRID]
-    group["geometry"] = "R"
-    group["rmax"] = grid.rmax
-    group["Nr"] = grid.Nr
-    return nothing
-end
-
-
-function _write_group_grid(fp, grid::Grids.GridRn)
-    HDF5.g_create(fp, GROUP_GRID)
-    group = fp[GROUP_GRID]
-    group["geometry"] = "Rn"
-    group["r"] = grid.r
-    return nothing
-end
-
-
-function _write_group_grid(fp, grid::Grids.GridT)
-    HDF5.g_create(fp, GROUP_GRID)
-    group = fp[GROUP_GRID]
-    group["geometry"] = "T"
-    group["tmin"] = grid.tmin
-    group["tmax"] = grid.tmax
-    group["Nt"] = grid.Nt
-    return nothing
-end
-
-
-function _write_group_grid(fp, grid::Grids.GridRT)
-    HDF5.g_create(fp, GROUP_GRID)
-    group = fp[GROUP_GRID]
-    group["geometry"] = "RT"
-    group["rmax"] = grid.rmax
-    group["Nr"] = grid.Nr
-    group["tmin"] = grid.tmin
-    group["tmax"] = grid.tmax
-    group["Nt"] = grid.Nt
-    return nothing
-end
-
-
-function _write_group_grid(fp, grid::Grids.GridXY)
-    HDF5.g_create(fp, GROUP_GRID)
-    group = fp[GROUP_GRID]
-    group["geometry"] = "XY"
-    group["xmin"] = grid.xmin
-    group["xmax"] = grid.xmax
-    group["Nx"] = grid.Nx
-    group["ymin"] = grid.ymin
-    group["ymax"] = grid.ymax
-    group["Ny"] = grid.Ny
-    return nothing
-end
-
-
-function _write_group_grid(fp, grid::Grids.GridXYT)
-    HDF5.g_create(fp, GROUP_GRID)
-    group = fp[GROUP_GRID]
-    group["geometry"] = "XYT"
-    group["xmin"] = grid.xmin
-    group["xmax"] = grid.xmax
-    group["Nx"] = grid.Nx
-    group["ymin"] = grid.ymin
-    group["ymax"] = grid.ymax
-    group["Ny"] = grid.Ny
-    group["tmin"] = grid.tmin
-    group["tmax"] = grid.tmax
-    group["Nt"] = grid.Nt
-    return nothing
-end
-
-
-function _write_group_zdat(fp, grid::Grids.GridRT)
-    HDF5.g_create(fp, GROUP_ZDAT)
-    group = fp[GROUP_ZDAT]
-
-    Nw = length(FFTW.rfftfreq(grid.Nt))
-
-    d_create(group, "z", FloatGPU, ((1,), (-1,)))
-    # d_create(group, "Fzx", FloatGPU, ((1, grid.Nr), (-1, grid.Nr)))
-    d_create(group, "Fzx", FloatGPU, ((grid.Nr, 1), (grid.Nr, -1)))
-    d_create(group, "Ft", FloatGPU, ((grid.Nt, 1), (grid.Nt, -1)))
-    # d_create(group, "Nezx", FloatGPU, ((1, grid.Nr), (-1, grid.Nr)))
-    d_create(group, "Nezx", FloatGPU, ((grid.Nr, 1), (grid.Nr, -1)))
-    # d_create(group, "iSzf", FloatGPU, ((1, Nw), (-1, Nw)))
-    d_create(group, "iSzf", FloatGPU, ((Nw, 1), (Nw, -1)))
-    return nothing
-end
-
-
-function _write_group_zdat(fp, grid::Grids.GridXY)
-    HDF5.g_create(fp, GROUP_ZDAT)
-    group = fp[GROUP_ZDAT]
-    d_create(group, "z", FloatGPU, ((1,), (-1,)))
-    d_create(group, "Izx", FloatGPU, ((1, grid.Nx), (-1, grid.Nx)))
-    d_create(group, "Izy", FloatGPU, ((1, grid.Ny), (-1, grid.Ny)))
-    return nothing
-end
-
-
 function writeHDF(
     plothdf::PlotHDF,
     field::Fields.Field,
     analyzer::FieldAnalyzers.FieldAnalyzer,
-    z::T,
-) where T<:AbstractFloat
+    z::AbstractFloat,
+)
     if (z != plothdf.zprev_field) & (z >= plothdf.znext_field)
-        dset = "$(Formatting.fmt("03d", plothdf.ifield))"
-        println(" Writing dataset $(dset)...")
-
-        fp = HDF5.h5open(plothdf.fname, "r+")
-        group_fdat = fp[GROUP_FDAT]
-        if plothdf.geometry <: Grids.GridR
-            write_field_r(group_fdat, dset, field)
-        elseif plothdf.geometry <: Grids.GridRn
-            write_field_rn(group_fdat, dset, field)
-        elseif plothdf.geometry <: Grids.GridT
-            write_field_t(group_fdat, dset, field)
-        elseif plothdf.geometry <: Grids.GridRT
-            write_field_rt(group_fdat, dset, field)
-        elseif plothdf.geometry <: Grids.GridXY
-            write_field_xy(group_fdat, dset, field)
-        elseif plothdf.geometry <: Grids.GridXYT
-            write_field_xyt(group_fdat, dset, field)
-        else
-            error("Wrong grid geometry.")
-        end
-        HDF5.attrs(group_fdat[dset])["z"] = z
-        HDF5.close(fp)
-
+        _write_field(plothdf, field, z)
         plothdf.ifield = plothdf.ifield + 1
         plothdf.znext_field = plothdf.znext_field + plothdf.dz_field
     end
 
     if plothdf.ZDATA
         if z >= plothdf.znext_zdata
-            writeHDF_zdata(plothdf, analyzer)
+            _write_zdata(plothdf, analyzer)
             plothdf.izdata = plothdf.izdata + 1
             plothdf.zprev_field = z
             plothdf.znext_zdata = z + plothdf.dz_zdata
@@ -486,7 +305,81 @@ function writeHDF(
 end
 
 
-function writeHDF_zdata(
+function _write_field(plothdf::PlotHDF, field::Fields.Field, z::AbstractFloat)
+    dset = "$(Formatting.fmt("03d", plothdf.ifield))"
+    println(" Writing dataset $(dset)...")
+
+    fp = HDF5.h5open(plothdf.fname, "r+")
+    group = fp[GROUP_FDAT]
+    if plothdf.geometry <: Union{Grids.GridR, Grids.GridRn, Grids.GridT}
+        group[dset] = Array(field.E)
+    elseif plothdf.geometry <: Grids.GridXY
+        # Compression is too slow, while the difference in the final file size
+        # is small
+        # E = Array(field.E)
+        # chunk = guess_chunk(size(E), sizeof(eltype(E)))
+        # group[dset, "chunk", chunk, "shuffle", (), "compress", 9] = E
+        group[dset] = Array(field.E)
+    elseif plothdf.geometry <: Grids.GridRT
+        E = Array(real.(field.E))
+        chunk = guess_chunk(size(E), sizeof(eltype(E)))
+        group[dset, "chunk", chunk, "shuffle", (), "compress", 9] = E
+    elseif plothdf.geometry <: Grids.GridXYT
+        group[dset] = Array(real.(field.E))
+    else
+        error("Wrong grid geometry.")
+    end
+    HDF5.attrs(group[dset])["z"] = z
+    HDF5.close(fp)
+
+    return nothing
+end
+
+
+function _group_unit(fp, unit::Units.Unit)
+    HDF5.g_create(fp, GROUP_UNIT)
+    group = fp[GROUP_UNIT]
+    for name in fieldnames(typeof(unit))
+        group[String(name)] = getfield(unit, name)
+    end
+    return nothing
+end
+
+
+function _group_grid(fp, grid::Grids.Grid)
+    HDF5.g_create(fp, GROUP_GRID)
+    group = fp[GROUP_GRID]
+    for name in fieldnames(typeof(grid))
+        data = getfield(grid, name)
+        if typeof(data) <: AbstractRange
+            group[String(name)] = Array(data)
+        else
+            group[String(name)] = data
+        end
+    end
+    return nothing
+end
+
+
+# ------------------------------------------------------------------------------
+# RT
+# ------------------------------------------------------------------------------
+function _group_zdat(fp, grid::Grids.GridRT)
+    HDF5.g_create(fp, GROUP_ZDAT)
+    group = fp[GROUP_ZDAT]
+
+    Nw = length(FFTW.rfftfreq(grid.Nt))
+
+    d_create(group, "z", FloatGPU, ((1,), (-1,)))
+    d_create(group, "Fzr", FloatGPU, ((1, grid.Nr), (-1, grid.Nr)))
+    d_create(group, "rhozr", FloatGPU, ((1, grid.Nr), (-1, grid.Nr)))
+    d_create(group, "Fzt", FloatGPU, ((1, grid.Nt), (-1, grid.Nt)))
+    d_create(group, "iSzf", FloatGPU, ((1, Nw), (-1, Nw)))
+    return nothing
+end
+
+
+function _write_zdata(
     plothdf::PlotHDF, analyzer::FieldAnalyzers.FieldAnalyzerRT,
 )
     iz = plothdf.izdata
@@ -499,27 +392,21 @@ function writeHDF_zdata(
     HDF5.set_dims!(data, (iz,))
     data[iz] = analyzer.z
 
-    data = group_zdat["Fzx"]
-    # HDF5.set_dims!(data, (iz, grid.Nr))
-    # data[iz, :] = analyzer.F
-    HDF5.set_dims!(data, (length(analyzer.Fr), iz))
-    data[:, iz] = analyzer.Fr
+    data = group_zdat["Fzr"]
+    HDF5.set_dims!(data, (iz, length(analyzer.Fr)))
+    data[iz, :] = analyzer.Fr
 
-    data = group_zdat["Ft"]
-    HDF5.set_dims!(data, (length(analyzer.Ft), iz))
-    data[:, iz] = analyzer.Ft
+    data = group_zdat["Fzt"]
+    HDF5.set_dims!(data, (iz, length(analyzer.Ft)))
+    data[iz, :] = analyzer.Ft
 
-    data = group_zdat["Nezx"]
-    # HDF5.set_dims!(data, (iz, grid.Nr))
-    # data[iz, :] = analyzer.rho
-    HDF5.set_dims!(data, (length(analyzer.rho), iz))
-    data[:, iz] = analyzer.rho
+    data = group_zdat["rhozr"]
+    HDF5.set_dims!(data, (iz, length(analyzer.rho)))
+    data[iz, :] = analyzer.rho
 
     data = group_zdat["iSzf"]
-    # HDF5.set_dims!(data, (iz, length(analyzer.S)))
-    # data[iz, :] = analyzer.S
-    HDF5.set_dims!(data, (length(analyzer.S), iz))
-    data[:, iz] = analyzer.S
+    HDF5.set_dims!(data, (iz, length(analyzer.S)))
+    data[iz, :] = analyzer.S
 
     HDF5.close(fp)
 
@@ -527,7 +414,20 @@ function writeHDF_zdata(
 end
 
 
-function writeHDF_zdata(
+# ------------------------------------------------------------------------------
+# XY
+# ------------------------------------------------------------------------------
+function _group_zdat(fp, grid::Grids.GridXY)
+    HDF5.g_create(fp, GROUP_ZDAT)
+    group = fp[GROUP_ZDAT]
+    d_create(group, "z", FloatGPU, ((1,), (-1,)))
+    d_create(group, "Izx", FloatGPU, ((1, grid.Nx), (-1, grid.Nx)))
+    d_create(group, "Izy", FloatGPU, ((1, grid.Ny), (-1, grid.Ny)))
+    return nothing
+end
+
+
+function _write_zdata(
     plothdf::PlotHDF, analyzer::FieldAnalyzers.FieldAnalyzerXY,
 )
     iz = plothdf.izdata
@@ -557,51 +457,7 @@ function writeHDF_zdata(
 end
 
 
-function write_field_r(group, dataset, field::Fields.Field)
-    group[dataset] = CUDA.collect(field.E)
-    return nothing
-end
-
-
-function write_field_rn(group, dataset, field::Fields.Field)
-    group[dataset] = field.E
-    return nothing
-end
-
-
-function write_field_t(group, dataset, field::Fields.Field)
-    group[dataset] = field.E
-    return nothing
-end
-
-
-function write_field_rt(group, dataset, field::Fields.Field)
-    E = CUDA.collect(transpose(real.(field.E)))
-    shape = size(E)
-    typesize = sizeof(eltype(E))
-    chunk = guess_chunk(shape, typesize)
-    group[dataset, "chunk", chunk, "shuffle", (), "compress", 9] = E
-    return nothing
-end
-
-
-function write_field_xy(group, dataset, field::Fields.Field)
-    group[dataset] = CUDA.collect(transpose(field.E))
-    return nothing
-end
-
-
-function write_field_xyt(group, dataset, field::Fields.Field)
-    # E = CUDA.collect(real.(field.E))
-    # shape = size(E)
-    # typesize = sizeof(eltype(E))
-    # chunk = guess_chunk(shape, typesize)
-    # group[dataset, "chunk", chunk, "shuffle", (), "compress", 9] = E
-    group[dataset] = CUDA.collect(real.(field.E))
-    return nothing
-end
-
-
+# ------------------------------------------------------------------------------
 """
 Guess an appropriate chunk layout for a dataset, given its shape and the size of
 each element in bytes. Will allocate chunks only as large as MAX_SIZE. Chunks
@@ -668,10 +524,12 @@ function guess_chunk(shape, typesize)
 end
 
 
-function d_create(parent::Union{HDF5.HDF5File, HDF5.HDF5Group},
-                  path::String,
-                  dtype::Type,
-                  dspace_dims::Tuple{Dims,Dims})
+function d_create(
+    parent::Union{HDF5.HDF5File, HDF5.HDF5Group},
+    path::String,
+    dtype::Type,
+    dspace_dims::Tuple{Dims, Dims},
+)
      shape = dspace_dims[2]
      typesize = sizeof(dtype)
      chunk = guess_chunk(shape, typesize)
