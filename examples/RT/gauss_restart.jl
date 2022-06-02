aca3897af3beda63832451038c9aa7b635303ec1
iamfedoroff <iamfedoroff@gmail.com>
2022-06-02 14:39:47 +0300

    Update the example restart file

--- examples/RT/gauss_restart.jl
+++ examples/RT/gauss_restart.jl
@@ -3,21 +3,20 @@
 import HDF5
 
 # HDF file with the field (calculated on the same grid):
-fname = "./results/plot.h5"
-dset = "001"   # dataset representing the field at a specific distance
+fname = "./results/ok_plot.h5"
+dset = "015"   # dataset representing the field at a specific distance
 
 # read the field data:
 pf = HDF5.h5open(fname, "r")
 Edata = pf["field/" * dset]
 
 # the distance at which the initial condition is defined:
-z = HDF5.read(HDF5.a_open(Edata, "z"))
+z = HDF5.read(HDF5.attributes(Edata)["z"])
 z = z * HDF5.read(pf["units/z"])   # convert to SI units
 
-lam0 = 800e-9   # [m] central wavelength
+lam0 = 0.8e-6   # [m] central wavelength
 
 
 function initial_condition(r, t, ru, tu, Iu)
-    E = HDF5.read(Edata)
-    return transpose(E)
+    return HDF5.read(Edata)
 end
